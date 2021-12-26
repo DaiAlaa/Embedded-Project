061e706b79abd18165530ebfd6fe953880c4efdf
AbdallahHussien4 <Abdalah.Abdelhameed98@eng-st.cu.edu.eg>
2021-12-26 17:30:49 +0200

    Add comments

--- Integration/integration/integration.ino
+++ Integration/integration/integration.ino
@@ -8,31 +8,32 @@ const int rs = A0, en = A1, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 ///////// IR pins  //////////
-int IRSensor = 2; // connect ir sensor to arduino pin 2
-int IRSensorClose=4; // connect ir sensor to arduino pin 2
-////////// RFID ///////////
-#include <SPI.h>
-#include <MFRC522.h>
+int IRSensor = 2; // connect first ir sensor to arduino pin 2
+int IRSensorClose=4; // connect second ir sensor to arduino pin 4
 
-#define RST_PIN         5          // Configurable, see typical pin layout above
-#define SS_PIN          10         // Configurable, see typical pin layout above
-#define RED_PIN          9
-#define GREEN_PIN        8
+////////// RFID ///////////
+#include <SPI.h>                // include SPI library to communicate with RFID.
+#include <MFRC522.h>            // include RFID library to use its related functions.
+
+  
+#define RST_PIN         5          
+#define SS_PIN          10         
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+#define RED_PIN          9      // Set Red led pin
+#define GREEN_PIN        8      // Set Green led pin
 
 /////// servo pins /////////
 #include <Servo.h> // include servo library to use its related functions
-#define Servo_PWM 6 // A descriptive name for D6 pin of Arduino to provide PWM signal
+#define Servo_PWM 6 // pin 6 of Arduino to provide PWM signal
 Servo SG90_Servo;  // Define an instance of of Servo with the name of "SG90_Servo"
 
-String BlueCard = " 167 238 66 178";
+
+String BlueCard = " 167 238 66 178";    // Authenticated Card
 double scanMillis;
 bool passed = false;
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
-
 //////////// return card id ////////////
 String getCardID(MFRC522 object) {
-//  Serial.print(F("Card UID:"));
   String cardId="";
   for (byte i = 0; i < object.uid.size; i++) {
     if(object.uid.uidByte[i] < 0x10)
@@ -40,24 +41,22 @@ String getCardID(MFRC522 object) {
     else
       cardId+=(F(" "));
     cardId+=object.uid.uidByte[i];
-   //Serial.print(object.uid.uidByte[i]);
   } 
   return cardId;
- 
 } 
 
 void setup() 
 {
   Serial.begin(9600);    // Initialize serial communications with the PC
-  pinMode (IRSensor, INPUT); // sensor pin INPUT
+  pinMode (IRSensor, INPUT); // IR sensor pin INPUT
   pinMode (RED_PIN, OUTPUT); // Led pin OUTPUT
   pinMode (GREEN_PIN, OUTPUT); // Led pin OUTPUT
-  pinMode (IRSensorClose, INPUT); // sensor pin INPUT
+  pinMode (IRSensorClose, INPUT); // IR sensor pin INPUT
+  
   //////// LCD setup  ////////
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-
+  
   //////// RFID setup /////////
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   SPI.begin();      // Init SPI bus
@@ -79,6 +78,7 @@ void loop()
   int statusSensor = digitalRead (IRSensor);
   int closeStatusSensor=digitalRead(IRSensorClose);
   lcd.setCursor(0, 0);
+  // Exit State --> a car exits the gate. 
   if(!closeStatusSensor){
     digitalWrite(RED_PIN, LOW);
     digitalWrite(GREEN_PIN, LOW);
@@ -88,6 +88,7 @@ void loop()
   }
   else {
     
+    // Idle state --> there is no car in front of the gate
     if (statusSensor == 1  && passed==false)
     {
       digitalWrite(RED_PIN, LOW);
@@ -97,6 +98,7 @@ void loop()
       passed = false;
     }
     
+    // Car State --> there is a car in front of the gate
     else if(statusSensor == 0  && passed==false)
     
     {
@@ -114,6 +116,7 @@ void loop()
         String s = getCardID(mfrc522);
         
         scanMillis = millis();
+        // Pass State --> Scanned authenticated card
         if(s == BlueCard){
           Serial.println("correct access");
           lcd.setCursor(0, 0);
@@ -123,6 +126,7 @@ void loop()
           passed = true;
   
         }
+        // Fail State --> Scanned unauthenticated card
         else{
           Serial.println("incorrect access");
           lcd.setCursor(0, 0);
@@ -131,11 +135,7 @@ void loop()
           digitalWrite(RED_PIN, HIGH);
           while(millis()-scanMillis <3000);
           digitalWrite(RED_PIN, LOW);
-
-          
           }
-  //      Serial.println("3aaaaaaaaaaa");
-  //      mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
     }
   }
 }
